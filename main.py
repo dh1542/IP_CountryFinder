f426df2260510a09c186b7bed8a085859868e11c
Dominik <Dominik@mbp>
2021-03-14 15:49:59 +0100

    Added validation for ipv4 ipv6 addresses

---
+++ main.py
@@ -0,0 +1,67 @@
+import re
+
+
+# Country from IP Lookup – Enter an IP address and find the country that IP is registered in.
+
+
+# A valid IPv4 address is an IP in the form "x1.x2.x3.x4" where 0 <= xi <= 255 and xi cannot contain leading zeros. For example, 
+# "192.168.1.1" and "192.168.1.0" are valid IPv4 addresses but "192.168.01.1", while "192.168.1.00" and "192.168@1.1" are invalid IPv4 addresses.
+
+
+
+# Method to validate ip address
+def validate_IP(IP):
+	if "." in IP:
+		nums = IP.split('.')
+		if(len(nums) == 4):
+			for e in nums:
+
+				if(e.isnumeric() == False):
+					return False
+				elif(len(e) != 1 and int(e) == 0):
+					return False
+				elif(len(e) >= 4 or len(e) <= 0 or int(e) > 255 or int(e) < 0):
+					return False
+				
+			return 4
+		else:
+			return False
+	# A valid IPv6 address is an IP in the form "x1:x2:x3:x4:x5:x6:x7:x8" where: 1 <= xi.length <= 4xi is a hexadecimal string which may contain digits, 
+	# lower-case English letter ('a' to 'f') and upper-case English letters ('A' to 'F'). Leading zeros are allowed in xi.
+	elif ":" in IP:
+		nums = IP.split(':')
+		if(len(nums) == 8):
+			for e in nums:
+				if(len(e) <= 0 or len(e) > 4):
+					return False
+				if(e.isnumeric() == False):
+					if(re.search('[a-fA-F]', e) == False):
+						return False
+			return 6
+
+	else:
+		return False
+
+				
+
+		
+
+		
+
+	
+
+
+
+ip = "02001:0db8:85a3:0000:0000:8a2e:0370:7334"
+print(validate_IP(ip))
+
+
+
+
+
+
+
+
+
+
+
